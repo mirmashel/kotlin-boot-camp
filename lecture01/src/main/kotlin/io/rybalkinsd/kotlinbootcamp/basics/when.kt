593d74513e8dd1ff1051b70c10f4fdad39b8ca2e
mirmashel <maximkras505@gmail.com>
2018-09-26 23:03:14 +0300

    dd

--- lecture01/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/basics/when.kt
+++ lecture01/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/basics/when.kt
@@ -2,7 +2,7 @@ package io.rybalkinsd.kotlinbootcamp.basics
 
 fun countDigits(string: String): String {
     return when (string.toInt()) {
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9 -> "1 digit"
+        7, 8, 9 -> "1 digit"
         in 10..99 -> "2 digits"
         else -> "many digits"
     }
