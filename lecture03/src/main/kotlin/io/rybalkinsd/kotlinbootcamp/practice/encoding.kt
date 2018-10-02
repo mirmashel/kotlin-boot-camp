5787b1a119213452c0bf43a6a6f7eb613907e38e
mirmashel <maximkras505@gmail.com>
2018-10-02 20:11:21 +0300

    lec

--- lecture03/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/practice/encoding.kt
+++ lecture03/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/practice/encoding.kt
@@ -4,13 +4,15 @@ package io.rybalkinsd.kotlinbootcamp.practice
 /**
  * NATO phonetic alphabet
  */
-val alphabet = setOf("Alfa", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliett", "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform", "Victor", "Whiskey", "Xray", "Yankee", "Zulu")
+val alphabet = setOf("Alfa", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India",
+        "Juliett", "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo", "Sierra",
+        "Tango", "Uniform", "Victor", "Whiskey", "Xray", "Yankee", "Zulu")
 
 /**
  * A mapping for english characters to phonetic alphabet.
  * [ a -> Alfa, b -> Bravo, ...]
  */
-val association: Map<Char, String> = TODO()
+val association: Map<Char, String> = alphabet.associateBy { it[0].toLowerCase() }
 
 /**
  * Extension function for String which encode it according to `association` mapping
@@ -21,13 +23,15 @@ val association: Map<Char, String> = TODO()
  * "abc".encode() == "AlfaBravoCharlie"
  *
  */
-fun String.encode(): String = TODO()
-
+fun String.encode(): String =
+        map { it.toLowerCase() }.
+        map { association[it] ?: it }
+        .joinToString("")
 /**
  * A reversed mapping for association
  * [ alpha -> a, bravo -> b, ...]
  */
-val reversedAssociation: Map<String, Char> = TODO()
+val reversedAssociation: Map<String, Char> = emptyMap()
 
 
 /**
