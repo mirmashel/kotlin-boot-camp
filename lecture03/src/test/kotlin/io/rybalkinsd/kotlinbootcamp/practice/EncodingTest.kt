b4e21adbc6098445546287ad70c13107312dbf8a
mirmashel <maximkras505@gmail.com>
2018-10-03 17:24:45 +0300

    decodeee

--- lecture03/src/test/kotlin/io/rybalkinsd/kotlinbootcamp/practice/EncodingTest.kt
+++ lecture03/src/test/kotlin/io/rybalkinsd/kotlinbootcamp/practice/EncodingTest.kt
@@ -40,10 +40,6 @@ class EncodingTest {
     fun `decode # abc`() {
         assertEquals("abc", "AlfaBravoCharlie".decode())
     }
-    @Test
-    fun `decode # Abc`() {
-        assertEquals("Abc", "AlfaBravoCharlie".decode())
-    }
 
     @Test
     fun `decode # AlfaBravo 123Charlie`() {
